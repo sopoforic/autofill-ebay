985999b23cf72b5fd91e69816205a2422dab0a68
Tracy Poff <sopoforic@users.noreply.github.com>
2010-01-30 04:24:18 -0500

    Added another page. Version 0.1.2.

--- autofillebaymaximumbid.user.js
+++ autofillebaymaximumbid.user.js
@@ -2,7 +2,7 @@
 // @name           Autofill eBay Maximum Bid
 // @namespace      http://sopoforic.wordpress.com/
 // @description    Automatically fills in the maximum bid box with the minimum possible bid.
-// @version        0.1.1
+// @version        0.1.2
 // @copyright      2010+, Tracy Poff (http://sopoforic.wordpress.com/)
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @include        *.ebay.tld/*
@@ -25,6 +25,13 @@ else if (MaxBidElement = document.getElementById("v4-30")){
 	var maxbid_box = document.getElementById("v4-13");
 	if(maxbid_box){maxbid_box.value = amount;} 
 }
+else if (MaxBidElement = document.getElementById("v4-31")){
+	//US currency, watched item
+	MaxBidElement.innerHTML.match(/Enter .+(\d+\.\d+) or more/);
+	amount = RegExp.$1;
+	var maxbid_box = document.getElementById("v4-14");
+	if(maxbid_box){maxbid_box.value = amount;} 
+}
 else if (MaxBidElement = document.getElementById("v4-32")){
 	//US currency outbid page
 	MaxBidElement.innerHTML.match(/Enter .+(\d+\.\d+) or more/);
@@ -35,6 +42,8 @@ else if (MaxBidElement = document.getElementById("v4-32")){
 	}
 	else if (maxbid_box = document.getElementById("v4-31")){
 		//US currency, little time left (1-click available)
+		//That ID is also used for the *amount* on a watched item. Bad, bad ebay.
+		//May have to find a better way to distinguish these, if possible.
 		maxbid_box.value = amount;
 	}
 }
