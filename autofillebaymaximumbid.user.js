56cc296685dd06d61c80947cf82db564bcd28832
Tracy Poff <sopoforic@users.noreply.github.com>
2010-02-02 08:52:37 -0500

    Regex improvements for US currency.

--- autofillebaymaximumbid.user.js
+++ autofillebaymaximumbid.user.js
@@ -2,7 +2,7 @@
 // @name           Autofill eBay Maximum Bid
 // @namespace      http://sopoforic.wordpress.com/
 // @description    Automatically fills in the maximum bid box with the minimum possible bid.
-// @version        0.1.2
+// @version        0.1.3
 // @copyright      2010+, Tracy Poff (http://sopoforic.wordpress.com/)
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @include        *.ebay.tld/*
@@ -13,7 +13,7 @@ var amount;
 
 if (MaxBidElement = document.getElementById("v4-29")){
 	//US currency normal page
-	MaxBidElement.innerHTML.match(/Enter .+(\d+\.\d+) or more/);
+	MaxBidElement.innerHTML.match(/Enter US \$(\d+\.\d+) or more/);
 	amount = RegExp.$1;
 	var maxbid_box = document.getElementById("v4-12");
 	if(maxbid_box){maxbid_box.value = amount;} 
@@ -27,14 +27,14 @@ else if (MaxBidElement = document.getElementById("v4-30")){
 }
 else if (MaxBidElement = document.getElementById("v4-31")){
 	//US currency, watched item
-	MaxBidElement.innerHTML.match(/Enter .+(\d+\.\d+) or more/);
+	MaxBidElement.innerHTML.match(/Enter US \$(\d+\.\d+) or more/);
 	amount = RegExp.$1;
 	var maxbid_box = document.getElementById("v4-14");
 	if(maxbid_box){maxbid_box.value = amount;} 
 }
 else if (MaxBidElement = document.getElementById("v4-32")){
 	//US currency outbid page
-	MaxBidElement.innerHTML.match(/Enter .+(\d+\.\d+) or more/);
+	MaxBidElement.innerHTML.match(/Enter US \$(\d+\.\d+) or more/);
 	amount = RegExp.$1;
 	var maxbid_box = document.getElementById("v4-15");
 	if(maxbid_box){
