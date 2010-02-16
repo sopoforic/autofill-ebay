05125a392c23ab88b867d49ba5bf3f4bd3a19b7a
Tracy Poff <tracy.poff@gmail.com>
2010-02-15 20:28:59 -0500

    Simplified code dramatically. Should now work for most normal pages, if the currency is implemented. Version 0.2.0
    It turns out that there is a table surrounding the text I need, so I can safely use regex on the whole table for the bid amount. My testing suggests that there is no problem using getElementsByName in Firefox (at least my version), so I've switched to that for the bid box. Between these, it should mean I no longer need to deal with all the changing ids. Great!

--- autofillebaymaximumbid.user.js
+++ autofillebaymaximumbid.user.js
@@ -2,72 +2,26 @@
 // @name           Autofill eBay Maximum Bid
 // @namespace      http://sopoforic.wordpress.com/
 // @description    Automatically fills in the maximum bid box with the minimum possible bid.
-// @version        0.1.7
+// @version        0.2.0
 // @copyright      2010+, Tracy Poff (http://sopoforic.wordpress.com/)
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @include        *.ebay.tld/*
 // @require        http://sizzlemctwizzle.com/updater.php?id=67507
 // ==/UserScript==
 try{
-var MaxBidElement;
-var amount;
 
-if (MaxBidElement = document.getElementById("v4-23")){
-	//US currency, another page
-    if(MaxBidElement.innerHTML.match(/Enter US \$(\d+\.\d+) or more/)){
-        amount = RegExp.$1;
-        document.getElementById("v4-22").value = amount
+if (document.getElementById("vi-tTbl")) {
+    if (document.getElementById("vi-tTbl").innerHTML.match(/Enter US \$(\d+\.\d+) or more/)) {
+        document.getElementsByName("maxbid")[0].value = RegExp.$1;
     }
-    else if (MaxBidElement.innerHTML.match(/Geben Sie mindestens EUR (\d+\,\d+) ein/)) {
-        //from eBay.de, EUR currency
-        amount = RegExp.$1;
-        document.getElementById("v4-22").value = amount
+    else if (document.getElementById("vi-tTbl").innerHTML.match(/Geben Sie mindestens EUR (\d+\,\d+) ein/)) {
+        document.getElementsByName("maxbid")[0].value = RegExp.$1;
     }
-    else if (document.getElementById("v4-24").innerHTML.match(/Enter EUR (\d+\.\d+) or more/)) {
-        //EUR on ebay.com
-        amount = RegExp.$1;
-        document.getElementById("v4-23").value = amount
+    else if (document.getElementById("vi-tTbl").innerHTML.match(/Enter EUR (\d+\.\d+) or more/)) {
+        document.getElementsByName("maxbid")[0].value = RegExp.$1;
     }
-}
-else if (MaxBidElement = document.getElementById("v4-29")){
-	//US currency normal page
-	MaxBidElement.innerHTML.match(/Enter US \$(\d+\.\d+) or more/);
-	amount = RegExp.$1;
-	var maxbid_box = document.getElementsByTagName("maxbid");
-    //this isn't helping. Need a better way to do this.
-	if(maxbid_box){maxbid_box(0).value = amount;}
-}
-else if (MaxBidElement = document.getElementById("v4-30")){
-	//AU currency normal page
-	MaxBidElement.innerHTML.match(/Enter .+(\d+\.\d+) or more/);
-	amount = RegExp.$1;
-	var maxbid_box = document.getElementById("v4-13");
-	if(maxbid_box){maxbid_box.value = amount;} 
-}
-else if (MaxBidElement = document.getElementById("v4-31")){
-	//US currency, watched item
-	MaxBidElement.innerHTML.match(/Enter US \$(\d+\.\d+) or more/);
-	amount = RegExp.$1;
-	var maxbid_box = document.getElementById("v4-14");
-	if(maxbid_box){maxbid_box.value = amount;} 
-}
-else if (MaxBidElement = document.getElementById("v4-32")){
-	//US currency outbid page
-	MaxBidElement.innerHTML.match(/Enter US \$(\d+\.\d+) or more/);
-	amount = RegExp.$1;
-	var maxbid_box = document.getElementById("v4-15");
-	if(maxbid_box){
-		maxbid_box.value = amount;
-	}
-	else if (maxbid_box = document.getElementById("v4-31")){
-		//US currency, little time left (1-click available)
-		//That ID is also used for the *amount* on a watched item. Bad, bad ebay.
-		//May have to find a better way to distinguish these, if possible.
-		maxbid_box.value = amount;
-	}
 }
 }
 catch(err){
-	alert(err);
-
+    alert(err);
 }
