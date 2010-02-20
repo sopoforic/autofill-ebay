bdd771130e0405b5c180331d13bd83c2c2952d21
Tracy Poff <tracy.poff@gmail.com>
2010-02-19 20:55:08 -0500

    Added pounds in German. Version 0.2.1.

--- autofillebaymaximumbid.user.js
+++ autofillebaymaximumbid.user.js
@@ -1,8 +1,8 @@
-// ==UserScript==
+﻿// ==UserScript==
 // @name           Autofill eBay Maximum Bid
 // @namespace      http://sopoforic.wordpress.com/
 // @description    Automatically fills in the maximum bid box with the minimum possible bid.
-// @version        0.2.0
+// @version        0.2.1
 // @copyright      2010+, Tracy Poff (http://sopoforic.wordpress.com/)
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @include        *.ebay.tld/*
@@ -21,6 +21,9 @@ if (document.getElementById("vi-tTbl")) {
         document.getElementsByName("maxbid")[0].value = RegExp.$1;
     }
     else if (document.getElementById("vi-tTbl").innerHTML.match(/Enter GBP (\d+\.\d+) or more/)) {
+        document.getElementsByName("maxbid")[0].value = RegExp.$1;
+    }
+    else if (document.getElementById("vi-tTbl").innerHTML.match(/Geben Sie mindestens £(\d+\,\d+) ein/)) {
         document.getElementsByName("maxbid")[0].value = RegExp.$1;
     }
 }
