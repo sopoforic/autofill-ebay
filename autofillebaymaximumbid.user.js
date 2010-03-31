e998b5da9dffab2ffa51595cbeeec391030c7c9c
Tracy Poff <tracy.poff@gmail.com>
2010-03-30 23:43:09 -0400

    Support Australian dollars in English.

--- autofillebaymaximumbid.user.js
+++ autofillebaymaximumbid.user.js
@@ -2,7 +2,7 @@
 // @name           Autofill eBay Maximum Bid
 // @namespace      http://sopoforic.wordpress.com/
 // @description    Automatically fills in the maximum bid box with the minimum possible bid.
-// @version        0.3.1
+// @version        0.3.2
 // @copyright      2010+, Tracy Poff (http://sopoforic.wordpress.com/)
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @include        *.ebay.tld/*
@@ -16,6 +16,7 @@ var pattern = new Array();
 pattern.push(/Enter US \$(\d+\.\d+)/);
 pattern.push(/Enter EUR (\d+\.\d+)/);
 pattern.push(/Enter GBP (\d+\.\d+)/);
+pattern.push(/Enter AU \$(\d+\.\d+)/);
 
 //German patterns
 pattern.push(/Geben Sie mindestens EUR (\d+\,\d+)/);
