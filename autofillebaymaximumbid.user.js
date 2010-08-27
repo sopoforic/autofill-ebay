534d8ac562b8dc9d2b67c00bea5b0bb90f4ad147
Tracy Poff <tracy.poff@gmail.com>
2010-08-27 18:50:30 -0400

    Fixed filters to match on non-breaking spaces, too.

--- autofillebaymaximumbid.user.js
+++ autofillebaymaximumbid.user.js
@@ -2,7 +2,7 @@
 // @name           Autofill eBay Maximum Bid
 // @namespace      http://sopoforic.wordpress.com/
 // @description    Automatically fills in the maximum bid box with the minimum possible bid.
-// @version        0.3.2
+// @version        0.3.3
 // @copyright      2010+, Tracy Poff (http://sopoforic.wordpress.com/)
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @include        *.ebay.tld/*
@@ -13,14 +13,14 @@ try{
 var pattern = new Array();
 
 //English patterns
-pattern.push(/Enter US \$(\d+\.\d+)/);
-pattern.push(/Enter EUR (\d+\.\d+)/);
-pattern.push(/Enter GBP (\d+\.\d+)/);
-pattern.push(/Enter AU \$(\d+\.\d+)/);
+pattern.push(/Enter\sUS\s\$(\d+\.\d+)/);
+pattern.push(/Enter\sEUR\s(\d+\.\d+)/);
+pattern.push(/Enter\sGBP\s(\d+\.\d+)/);
+pattern.push(/Enter\sAU\s\$(\d+\.\d+)/);
 
 //German patterns
-pattern.push(/Geben Sie mindestens EUR (\d+\,\d+)/);
-pattern.push(/Geben Sie mindestens £(\d+\,\d+)/);
+pattern.push(/Geben\sSie\smindestens\sEUR (\d+\,\d+)/);
+pattern.push(/Geben\sSie\smindestenss£(\d+\,\d+)/);
 
 //Ordinary item page
 if (document.getElementById("vi-tTbl")) {
