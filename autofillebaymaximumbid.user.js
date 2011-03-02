706d0079feac564eaa3fc7ea1f5cebfc868edef2
Tracy Poff <tracy.poff@gmail.com>
2011-03-02 00:03:46 -0500

    New patterns and rewrote so it isn't page-specific. I think this doesn't make it less secure.

--- autofillebaymaximumbid.user.js
+++ autofillebaymaximumbid.user.js
@@ -2,7 +2,7 @@
 // @name           Autofill eBay Maximum Bid
 // @namespace      http://sopoforic.wordpress.com/
 // @description    Automatically fills in the maximum bid box with the minimum possible bid.
-// @version        0.3.3
+// @version        0.4.0
 // @copyright      2010+, Tracy Poff (http://sopoforic.wordpress.com/)
 // @license        GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @include        *.ebay.tld/*
@@ -13,31 +13,22 @@ try{
 var pattern = new Array();
 
 //English patterns
-pattern.push(/Enter\sUS\s\$(\d+\.\d+)/);
-pattern.push(/Enter\sEUR\s(\d+\.\d+)/);
-pattern.push(/Enter\sGBP\s(\d+\.\d+)/);
-pattern.push(/Enter\sAU\s\$(\d+\.\d+)/);
+pattern.push(/Enter US \$(\d+\.\d+)/);
+pattern.push(/Enter EUR (\d+\.\d+)/);
+pattern.push(/Enter GBP (\d+\.\d+)/);
+pattern.push(/Enter AU \$(\d+\.\d+)/);
 
 //German patterns
-pattern.push(/Geben\sSie\smindestens\sEUR (\d+\,\d+)/);
-pattern.push(/Geben\sSie\smindestens\s£(\d+\,\d+)/);
+pattern.push(/Geben Sie mindestens EUR (\d+\,\d+)/);
+pattern.push(/Geben Sie mindestens&nbsp;EUR (\d+\,\d+)/);
+pattern.push(/Geben Sie mindestens £(\d+\,\d+)/);
+pattern.push(/Geben Sie mindestens&nbsp;£(\d+\,\d+)/);
+pattern.push(/Geben Sie mindestens <span style="white-space: nowrap;">EUR (\d+\,\d+)/);
 
-//Ordinary item page
-if (document.getElementById("vi-tTbl")) {
-    for (x in pattern) {
-        if (document.getElementById("vi-tTbl").innerHTML.match(pattern[x])) {
-            document.getElementsByName("maxbid")[0].value = RegExp.$1;
-            break;
-        }
-    }
-}
-//"You've just been outbid" page
-else if (document.getElementsByName("OutbidForm")[0]) {
-    for (x in pattern) {
-        if (document.getElementsByName("OutbidForm")[0].innerHTML.match(pattern[x])) {
-            document.getElementsByName("maxbid")[0].value = RegExp.$1;
-            break;
-        }
+for (x in pattern) {
+    if (document.body.innerHTML.match(pattern[x])) {
+        document.getElementsByName("maxbid")[0].value = RegExp.$1;
+        break;
     }
 }
 }
